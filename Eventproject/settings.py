41d7ca526dac6ddffffea76c35ec6484de2f1346
@waminisce <onidareadewaleoluwafemi69@gmail.com>
2025-11-17 00:51:10 +0100

    Fix Pillow version and use Python 3.11

--- Eventproject/settings.py
+++ Eventproject/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-# import pymysql
-# pymysql.install_as_MySQLdb()
+import Eventproject.mysql_init
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
